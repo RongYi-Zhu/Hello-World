cac2fff2f0b9a2a6454db8c996cf046947654dc4
RongYi <cloud4450782@163.com>
2022-03-30 11:24:52 +0800

    modify line 51 change calculate way of sever time

--- Leetcode/Leet1606.cpp
+++ Leetcode/Leet1606.cpp
@@ -3,44 +3,68 @@
 #include<iostream>
 using namespace std;
 
-class Solution {
+class Solution 
+{
 public:
-    int GetCnt(string answerKey, char c, int k)
+    int SeekAva(vector<int> server, int time, int index)
     {
-        int i=0, j=k;
-        int cnt = 0;
-        int max = 0;
-
-        while(j < answerKey.size())
+        int num = 0, ans = index%server.size();
+        while(num < server.size())
         {
-            for(int ind=i; ind<j; ind++)
-                if (answerKey[ind] != c)
-                    cnt++;
-            
-            if (cnt <= k)
-            {
-                if (j-i > max)
-                    max = j-i;
-                j++;
-            }    
-            else
-            {
-                if (cnt > k)
-                    i++;
-            }
+            if (server[ans] <= time)
+                return ans;
+            num++;
+            ans = (ans+1)%server.size();
         }
-        return max;
+        return -1;
     }
 
-    int maxConsecutiveAnswers(string answerKey, int k) 
+    vector<int> VecMax(vector<int> arg)
     {
-       return max(GetCnt(answerKey,'T',k),GetCnt(answerKey,'F',k));
+        int max = 0;
+        vector<int> ans;
+        for(int i=0; i<arg.size(); i++)
+        {
+            if (arg[i] > max)
+            {
+                ans.clear();
+                ans.push_back(i);
+                max = arg[i];
+            }
+            else
+                if (arg[i] == max)
+                    ans.push_back(i);
+        }
+        return ans;
+    }
+
+    vector<int> busiestServers(int k, vector<int>& arrival, vector<int>& load) 
+    {
+        vector<int> server(k,0);
+        vector<int> num(k,0);
+
+        for(int i=0; i<arrival.size(); i++)
+        {
+            int ans = SeekAva(server, arrival[i], i);
+            if (ans > 0)
+            {
+                server[ans] = load[i] + arrival[i];
+                num[ans]++;
+            }       
+        }
+        return VecMax(num);
     }
 };
 
 int main()
 {
     Solution S1;
-    string str = "TTFF";
-    S1.maxConsecutiveAnswers(str,2);
+
+    //Data
+    int k = 3;
+    vector<int> arrival = {1,2,3,4};
+    vector<int> load = {1,2,1,2};
+    //Data
+
+    S1.busiestServers(k,arrival,load);
 }
